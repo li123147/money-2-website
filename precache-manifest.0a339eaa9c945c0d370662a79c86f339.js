ce232cfcf44ade63d86ad93224f2c13aa3485b61
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-07-06 10:57:14 +0800

    init

---
+++ precache-manifest.0a339eaa9c945c0d370662a79c86f339.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c6da17040e5f8b1a82e7f166d8ad67a",
+    "url": "/money-2-website/index.html"
+  },
+  {
+    "revision": "5c7f650bf11ee420da2e",
+    "url": "/money-2-website/static/css/main.339b420b.chunk.css"
+  },
+  {
+    "revision": "bac32a4a841f06cdacb4",
+    "url": "/money-2-website/static/js/2.480141f2.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/money-2-website/static/js/2.480141f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c7f650bf11ee420da2e",
+    "url": "/money-2-website/static/js/main.7b57a71e.chunk.js"
+  },
+  {
+    "revision": "f5eafa2833fb02116945",
+    "url": "/money-2-website/static/js/runtime-main.d3fd1a4f.js"
+  }
+]);
